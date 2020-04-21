bb276c21b7aa9ba928307d535a3e9219a1fe1b10
Vincent Brison <vincent.brison@fabernovel.com>
2020-04-21 16:28:54 +0200

    Improve desc output

--- desc/desc.sh
+++ desc/desc.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-echo "Apt packages:"
+echo "apt packages:"
 apt list --installed
-echo '\n'
+echo
 
-echo "Ruby env:"
+echo "rbenv:"
 rbenv -v
+echo
+
+echo "Default ruby version:"
 ruby -v
+echo
+
+echo "Installed gems:"
 gem list
-echo '\n'
+echo
 
 echo "gcloud env:"
 gcloud --version
-echo '\n'
+echo
 
 echo "Android env:"
 sdkmanager --sdk_root=$ANDROID_HOME --list
-echo '\n'
+echo
