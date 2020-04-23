6ce8a3c180e3d9a87f93e802119ad2fd87276556
Satyan Jacquens <satyan.jacquens@fabernovel.com>
2020-04-23 12:39:29 +0200

    Use the defined test app package name in the test

--- tests/test-firebase-test-lab/app/src/androidTest/java/com/fabernovel/test_firebase_test_lab/ExampleInstrumentedTest.kt
+++ tests/test-firebase-test-lab/app/src/androidTest/java/com/fabernovel/test_firebase_test_lab/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fabernovel.test_firebase_test_lab", appContext.packageName)
+        assertEquals("com.fabernovel.testapp", appContext.packageName)
     }
 }
