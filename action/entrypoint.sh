9260e979ba4c17463da10a2460a4ad57f93295a4
Vincent Brison <vincent.brison@fabernovel.com>
2020-05-04 18:04:56 +0200

    Cleanup

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,22 +2,15 @@
 image_tag=$1
 full_image_name="fabernovel/android:$1"
 echo "Full image name = $full_image_name"
+
 container_id=$(cat /etc/hostname)
 echo "Container id = $container_id"
-ci=${CI:-false}
-if [ "$ci" = true ]; then
-  echo "Running in CI"
-  volume_options="--volumes-from $container_id --workdir $GITHUB_WORKSPACE"
-else
-  echo "Not running in CI"
-  volume_options="-v $PWD:/wd --workdir /wd"
-fi
+volume_options="--volumes-from $container_id --workdir $GITHUB_WORKSPACE"
 
-echo "print workspace from docker-android"
 set -x
 docker run \
   $volume_options \
   -e HOME -e GITHUB_JOB -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_REPOSITORY_OWNER -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -e ACTIONS_RUNTIME_URL -e ACTIONS_RUNTIME_TOKEN -e ACTIONS_CACHE_URL -e GITHUB_ACTIONS=true -e CI=true \
   --rm $full_image_name \
   ls $GITHUB_WORKSPACE
-# TODO run docker with docker-android, then run in it android build test.
+# TODO Add ability to run command from action arg inside docker-android 
