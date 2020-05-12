229f686b90fb1a794141e002be4617b5fbef39a3
s.jcqs <satyan.jacquens@fabernovel.com>
2020-05-12 10:05:23 +0200

    Add shell action to run

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 image_tag=$1
-full_image_name="fabernovel/android:$1"
+exec=$2
+full_image_name="fabernovel/android:$image_tag"
 echo "Full image name = $full_image_name"
 
 container_id=$(cat /etc/hostname)
@@ -11,6 +12,6 @@ set -x
 docker run \
   $volume_options \
   -e HOME -e GITHUB_JOB -e GITHUB_REF -e GITHUB_SHA -e GITHUB_REPOSITORY -e GITHUB_REPOSITORY_OWNER -e GITHUB_RUN_ID -e GITHUB_RUN_NUMBER -e GITHUB_ACTOR -e GITHUB_WORKFLOW -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -e GITHUB_EVENT_NAME -e GITHUB_WORKSPACE -e GITHUB_ACTION -e GITHUB_EVENT_PATH -e RUNNER_OS -e RUNNER_TOOL_CACHE -e RUNNER_TEMP -e RUNNER_WORKSPACE -e ACTIONS_RUNTIME_URL -e ACTIONS_RUNTIME_TOKEN -e ACTIONS_CACHE_URL -e GITHUB_ACTIONS=true -e CI=true \
-  --rm $full_image_name \
-  ls $GITHUB_WORKSPACE
-# TODO Add ability to run command from action arg inside docker-android 
+  --rm \
+  $full_image_name \
+  $exec
