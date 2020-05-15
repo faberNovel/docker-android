bde159dafff664f823eedc5c667456dd3979818c
s.jcqs <satyan.jacquens@fabernovel.com>
2020-05-15 09:47:29 +0200

    Pretty print gcloud SDK versions

--- desc/desc.sh
+++ desc/desc.sh
@@ -9,8 +9,13 @@ echo "### Android SDKs"
 sdkmanager --list --sdk_root=$ANDROID_HOME |
     awk '/Installed packages:/{flag=1; next} /Available Packages/{flag=0} flag'
 echo "### Google Cloud SDK"
-gcloud --version
-echo
+echo "| Name | Version |"
+echo "|------|---------|"
+gcloud --version | while read -r lib; do
+    version=$(echo "${lib}" | awk '{ print $NF }')
+    name=$(echo $lib | awk '{$NF=""; print $0}')
+    echo "| $name | $version |"
+done
 
 echo "## APT packages"
 dpkg -l
