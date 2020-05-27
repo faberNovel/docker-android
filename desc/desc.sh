b40fb1618a7841f25b03f57e7f6084ce414237b1
s.jcqs <satyan.jacquens@fabernovel.com>
2020-05-27 14:21:01 +0200

    Don't print GCloud desc if not installed

--- desc/desc.sh
+++ desc/desc.sh
@@ -8,28 +8,31 @@ echo "## Android environment"
 echo "### Android SDKs"
 sdkmanager --list --sdk_root=$ANDROID_HOME |
     awk '/Installed packages:/{flag=1; next} /Available Packages/{flag=0} flag'
-echo "### Google Cloud SDK"
-echo "| Name | Version |"
-echo "|------|---------|"
-gcloud --version | while read -r lib; do
-    version=$(echo "${lib}" | awk '{ print $NF }')
-    name=$(echo $lib | awk '{$NF=""; print $0}')
-    echo "| $name | $version |"
-done
 
-echo "## Python environment"
-echo "### Python version"
-python3 --version | cut -d' ' -f2-
-echo "### PIP version"
-pip3 --version | cut -d' ' -f2-
-echo "### Installed PIP packages"
-echo "| Name | Version |"
-echo "|------|---------|"
-pip3 freeze | while read -r lib; do
-    version=$(echo "$lib" | awk -F'=='  '{print $2}')
-    name=$(echo "$lib" | awk -F'=='  '{print $1}')
-    echo "| $name | $version |"
-done
+if [ -n "$(which gcloud)" ]; then
+  echo "### Google Cloud SDK"
+  echo "| Name | Version |"
+  echo "|------|---------|"
+  gcloud --version | while read -r lib; do
+      version=$(echo "${lib}" | awk '{ print $NF }')
+      name=$(echo $lib | awk '{$NF=""; print $0}')
+      echo "| $name | $version |"
+  done
+
+  echo "## Python environment"
+  echo "### Python version"
+  python3 --version | cut -d' ' -f2-
+  echo "### PIP version"
+  pip3 --version | cut -d' ' -f2-
+  echo "### Installed PIP packages"
+  echo "| Name | Version |"
+  echo "|------|---------|"
+  pip3 freeze | while read -r lib; do
+      version=$(echo "$lib" | awk -F'=='  '{print $2}')
+      name=$(echo "$lib" | awk -F'=='  '{print $1}')
+      echo "| $name | $version |"
+  done
+fi
 
 echo "## Ruby environment"
 echo "### Default ruby version"
@@ -51,4 +54,3 @@ echo "| Name | Version | Architecture | Description |"
 echo "| ---- | ------- | ------------ | ----------- |"
 dpkg_format='| ${binary:Package} | ${Version} | ${Architecture} | ${binary:Summary} |\n'
 dpkg-query --show --showformat="$dpkg_format"
-done
