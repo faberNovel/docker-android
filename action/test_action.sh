73527a952ab3602affdc27092294f9e79a682015
Vincent Brison <vincent.brison@fabernovel.com>
2020-07-30 14:32:07 +0200

    Add more contextualized tests to action testing

--- action/test_action.sh
+++ action/test_action.sh
@@ -3,5 +3,12 @@
 # Exit immediately if a command returns a non-zero status.
 set -e
 
+# Assert we are running in docker
+if [ ! -f /.dockerenv ]; then
+    exit 1
+fi
+
+# Check basic tools
 java -version
 rbenv -v
+adb version
